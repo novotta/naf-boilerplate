05c3373813dc2af742eaf31a692af7af89cdc93e
Justin Seidl <aceeightofspades@gmail.com>
2024-02-29 12:48:40 -0600

    Link to account view

--- client/src/components/accounts/list.jsx
+++ client/src/components/accounts/list.jsx
@@ -1,16 +1,22 @@
 // Dependencies
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 
 // Components
-import { formatNumber, Row } from '@narmi/design_system';
+import { formatNumber, Popover, Row } from '@narmi/design_system';
 
 // Accounts
 const Accounts = ({ accounts, accountType }) => {
+  const navigate = useNavigate();
   const favoriteAccounts = accounts.filter(account => account.favorited);
   const listAccounts = accounts.filter(account => account.type === accountType);
   const printedAccounts = accountType === 'favorite' ? favoriteAccounts : listAccounts;
+
+  const handleClick = (accountId) => () => {
+    navigate(`/accounts/${accountId}`);
+  };
 
   return (
     <>
@@ -25,7 +31,7 @@ const Accounts = ({ accounts, accountType }) => {
       </AccountGroupHeader>
       <div className="account-rows">
         {printedAccounts.map((account) => (
-          <AccountRow key={account.id} className="padding--y--m">
+          <AccountRow key={account.id} className="padding--y--m" onClick={handleClick(account.id)}>
             <Row alignItems="center" justifyContent="start" gapSize="l">
               <Row.Item>
                 <span role="button" tabIndex="0">
@@ -33,13 +39,28 @@ const Accounts = ({ accounts, accountType }) => {
                 </span>
               </Row.Item>
               <Row.Item shrink>
-                <div className="balance-options">
+                <BalanceOptions>
                   <span role="button" tabIndex="0">
                     <span className="margin--right--xxs fontColor--primary">
-                      {formatNumber((account.balances.primary / 100), 'currency')}
+                      {formatNumber(account.balances.primary / 100, "currency")}
                     </span>
                   </span>
-                </div>
+                  <div class="options" style={{width: 'auto', position: 'relative'}}>
+                    <Popover
+                      alignment="end"
+                      content={
+                        <div className="padding--all--m">📦 Any content</div>
+                      }
+                      onUserDismiss={function noRefCheck() {}}
+                      side="bottom"
+                      wrapperDisplay="inline-flex"
+                    >
+                      <div className="options-traffic-light  ">
+                        <span className="clickable narmi-icon-more-vertical"></span>
+                      </div>
+                    </Popover>
+                  </div>
+                </BalanceOptions>
               </Row.Item>
             </Row>
             {/* {name, nickname, id, number, type, product, updated_at, users, features, routing, loan_details, source, state, favorited, hidden, out_of_date, updated_from_source_at, check_micr, metadata, balances, fi_name, created_at, verified, fi_svg} */}
@@ -80,4 +101,16 @@ const GroupName = styled.div`
   display: flex;
   flex-direction: row;
   text-transform: capitalize;
+`;
+
+const BalanceOptions = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  * {
+    align-items: center;
+    color: var(--color-grey);
+    display: flex;
+    justify-content: center;
+  }
 `;
