a50ea45c20fd049fbbde85a6b30beb75b7e76c52
Justin Seidl <aceeightofspades@gmail.com>
2024-03-06 14:46:56 -0600

    Message Center

--- client/src/views/messages.jsx
+++ client/src/views/messages.jsx
@@ -1,5 +1,6 @@
 // Dependencies
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 
@@ -9,7 +10,7 @@ import {
 } from '../actions/threads';
 
 // Components
-import { Row } from '@narmi/design_system';
+import { ContentCard, Row } from '@narmi/design_system';
 
 // Messages
 const Messages = (props) => {
@@ -26,35 +27,51 @@ const Messages = (props) => {
   if (props.threads.data !== null) {
     return (
       <NarmiContainer>
-        <div>
-          <h1>Messages</h1>
-          {props.threads.data.map((thread) => (
-            <Row key={thread.id} alignItems="center" justifyContent="start" gapSize="l">
-              <Row.Item>
-                <div onClick={() => handleThreadClick(thread)}>
-                  <div>{thread.subject}</div>
-                  <div>{thread.updated_at}</div>
-                </div>
-              </Row.Item>
-            </Row>
-          ))}
-        </div>
-        <div>
-          <h1>Selected Message:</h1>
-          {selectedThread ? (
-            <div>
-              <p>Name: {selectedThread.subject}</p>
-              <p>Attribute: {selectedThread.updated_at}</p>
-              {selectedThread.messages.map((message) => (
-                <div key={message.id}>
-                  <p>{message.body}</p>
-                </div>
+        <Row alignItems="center" justifyContent="start" gapSize="l">
+          <Row.Item>
+            <MessageTitle className="fontWeight--bold">Message Center</MessageTitle>
+            <Link to="/">Back to Overview</Link>
+          </Row.Item>
+          <Row.Item>
+            <button>Add Message</button>
+          </Row.Item>
+        </Row>
+        <ContentCard kind="elevated" paddingSize="none">
+          <PageLayout>
+            <LeftLayout>
+              {props.threads.data.map((thread) => (
+                <Row key={thread.id} alignItems="center" justifyContent="start" gapSize="l">
+                  <Row.Item>
+                    {/* if selectedThread.id === thread.id, add class "selected" */}
+                    <ThreadItem
+                      className={selectedThread && selectedThread.id === thread.id ? 'selected' : ''}
+                      onClick={() => handleThreadClick(thread)}
+                    >
+                      <div className="fontWeight--bold">{thread.subject}</div>
+                      <div>{thread.updated_at}</div>
+                    </ThreadItem>
+                  </Row.Item>
+                </Row>
               ))}
-            </div>
-          ) : (
-            <p>No item selected</p>
-          )}
-        </div>
+            </LeftLayout>
+            <RightLayout>
+              <h1>Selected Message:</h1>
+              {selectedThread ? (
+                <div>
+                  <p>Name: {selectedThread.subject}</p>
+                  <p>Attribute: {selectedThread.updated_at}</p>
+                  {selectedThread.messages.map((message) => (
+                    <div key={message.id}>
+                      <p>{message.body}</p>
+                    </div>
+                  ))}
+                </div>
+              ) : (
+                <p>No item selected</p>
+              )}
+            </RightLayout>
+          </PageLayout>
+        </ContentCard>
       </NarmiContainer>
     )
   }
@@ -81,4 +98,39 @@ export default connect(mapStateToProps, mapDispatchToProps)(Messages);
 // Styles
 const NarmiContainer = styled.div`
   width: 1300px;
+`;
+
+const MessageTitle = styled.h1`
+  color: RGBA(var(--primary-accessible-color));
+  line-height: 1.2;
+  margin: 24px 0;
+`;
+
+const PageLayout = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-grow: 1;
+`;
+
+const LeftLayout = styled.div`
+  border-right: 1px solid #F1F3F6;
+  order: 1;
+  padding: 24px;
+  width: 400px;
+`;
+
+const RightLayout = styled.div`
+  flex-grow: 1;
+  order: 2;
+  padding: 24px;
+`;
+
+const ThreadItem = styled.div`
+  border-radius: 8px;
+  cursor: pointer;
+  padding: 16px;
+
+  &.selected {
+    background-color: #F1F3F6;
+  }
 `;
